1a026d4bba6bbfcd8b46de4618dd87be0f85f535
Chris Sherman <chris.t.sherman@gmail.com>
2012-12-28 18:56:24 -0500

    restructuring directory. created a examples directory for sample scripts.

--- examples/insert.py
+++ examples/insert.py
@@ -1,7 +1,6 @@
 from SOAPpy import SOAPProxy
 
-#dynamic elements of SOAP endpoint construction 
-username, password, instance, gliderecord = 'admin', 'explodev1234', 'osuitsmdev', 'GetLiveProfileId'
+username, password, instance, gliderecord = '', '', '', ''
 proxy = "https://%s:%s@%s.service-now.com/%s.do?SOAP" % (username, password, instance, gliderecord, )
 namespace = 'http://www.glidesoft.com/'
 
@@ -9,7 +8,11 @@ server = SOAPProxy(proxy,namespace)
 #limit record set? 
 start, end = 0, 2
 
-response = server.get(username='sherman.1206' )
-print response 
+print server.insert(
+    u_username='',
+    u_text="this is my kiosk contact data", 
+    u_osu_id='32345235235',
+    u_short_description = 'my custom short description' 
+)
 
 
